b2d056f9418d8ee7a1722538e740fadfe9ea05f2
Ha Sang <hhsgg2157@gmail.com>
2020-03-27 15:18:49 +0800

    Add app_dayNight theme

---
+++ app/src/main/java/io/mountx/wukung/theme/AppDayNightActivity.kt
@@ -0,0 +1,24 @@
+package io.mountx.wukung.theme
+
+import android.os.Build
+import android.os.Bundle
+import android.view.View
+import io.mountx.common.app.LogActivity
+import io.mountx.wukung.R
+import kotlinx.android.synthetic.main.activity_widget_set.*
+
+/**
+ * @author hhs
+ * Created on 2020/3/25
+ */
+class AppDayNightActivity : LogActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_widget_set)
+        setSupportActionBar(toolbar)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+          //  window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        }
+    }
+}
