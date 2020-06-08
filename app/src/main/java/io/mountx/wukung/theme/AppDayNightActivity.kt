76d6a09a02ac5c1fd43320ccfc94e5e27d135982
Ha Sang <hhsgg2157@gmail.com>
2020-06-08 20:37:10 +0800

    Extends MxActivity

--- app/src/main/java/io/mountx/wukung/theme/AppDayNightActivity.kt
+++ app/src/main/java/io/mountx/wukung/theme/AppDayNightActivity.kt
@@ -1,9 +1,7 @@
 package io.mountx.wukung.theme
 
-import android.os.Build
 import android.os.Bundle
-import android.view.View
-import io.mountx.common.app.LogActivity
+import io.mountx.common.app.MxActivity
 import io.mountx.wukung.R
 import kotlinx.android.synthetic.main.activity_widget_set.*
 
@@ -11,14 +9,12 @@ import kotlinx.android.synthetic.main.activity_widget_set.*
  * @author hhs
  * Created on 2020/3/25
  */
-class AppDayNightActivity : LogActivity() {
+class AppDayNightActivity : MxActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_widget_set)
         setSupportActionBar(toolbar)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-          //  window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
-        }
+        requestLightStatusBar()
     }
 }
--- app/src/main/java/io/mountx/wukung/theme/AppMaterialActivity.kt
+++ app/src/main/java/io/mountx/wukung/theme/AppMaterialActivity.kt
@@ -1,9 +1,7 @@
 package io.mountx.wukung.theme
 
-import android.os.Build
 import android.os.Bundle
-import android.view.View
-import io.mountx.common.app.LogActivity
+import io.mountx.common.app.MxActivity
 import io.mountx.wukung.R
 import kotlinx.android.synthetic.main.activity_material_widget_set.*
 
@@ -11,14 +9,12 @@ import kotlinx.android.synthetic.main.activity_material_widget_set.*
  * @author hhs
  * Created on 2020/3/26
  */
-class AppMaterialActivity : LogActivity() {
+class AppMaterialActivity : MxActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_material_widget_set)
         setSupportActionBar(toolbar)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
-        }
+        requestDefaultStatusBar()
     }
 }
